6b09b179770a55fd8bb85e896a0ab1a4a555ce68
marco <marco@marcobehler.com>
2020-09-06 14:08:15 +0200

    [initial commit] the next facebook

---
+++ src/main/java/com/marcobehler/App.java
@@ -0,0 +1,13 @@
+package com.marcobehler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
