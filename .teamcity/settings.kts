71fcc38f08f9ebdeebb7249ce3934c5174cf864b
marco <marco@marcobehler.com>
2020-09-07 10:25:18 +0200

    Playing with build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,6 +2,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
